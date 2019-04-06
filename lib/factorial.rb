af576071fbdf6134b5b804ab37e25073e48ee827
Erica Norris <norrise120@gmail.com>
2019-04-06 13:15:21 -0700

    Added time and space complexity

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -1,6 +1,6 @@
 # Computes factorial of the input number and returns it
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n), where n is the number that is entered into the method
+# Space complexity: O(1)
 def factorial(number)
   raise ArgumentError, "You must enter a number." if number == nil
   return 1 if number == 0 || number == 1
