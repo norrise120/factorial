764ed6e200ef6ec8da540b4154f6bdc7fd66a2d4
Erica Norris <norrise120@gmail.com>
2019-04-06 13:08:11 -0700

    Created factorial method to pass tests

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -2,5 +2,12 @@
 # Time complexity: ?
 # Space complexity: ?
 def factorial(number)
-  raise NotImplementedError
+  raise ArgumentError, "You must enter a number." if number == nil
+  return 1 if number == 0 || number == 1
+  result = 1
+  while number > 0
+    result *= number
+    number -= 1
+  end
+  return result
 end
